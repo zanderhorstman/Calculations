90b6088148bf948cdce1f6270eceb050c081ad14
zanderhorstman <zanderhorstman@gmail.com>
2020-08-25 10:26:37 -0400

    1.0

--- src/Calculate.java
+++ src/Calculate.java
@@ -3,16 +3,15 @@ import java.util.Scanner;
 public class Calculate {
     public static void main(String[] args) {
 
-        int one;
-        int two;
         String coding="Coding is ";
         String fun="Fun!";
         Scanner keybd = new Scanner(System.in);
-        System.out.println("Enter a number:");
-        one = keybd.nextInt();
-        System.out.println("Enter another number: ");
-        two = keybd.nextInt();
-        System.out.println("\n\n\nSum :" + (one+two));
+
+        System.out.print("Enter a number: ");
+        int one = keybd.nextInt();
+        System.out.print("Enter another number: ");
+        int two = keybd.nextInt();
+        System.out.println("\nSum : " + (one+two));
         System.out.println("Product : " + (one*two));
         System.out.println("Square1 : " + (one*one));
         System.out.println("Square2  : " + (two*two));
